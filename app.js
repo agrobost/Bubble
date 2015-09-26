44ab25f3b9816ed4fd8200da90310a9398e69acb
Romain <romain879@gmail.com>
2015-09-27 01:05:46 +0200

    Install of socket.io

--- app.js
+++ app.js
@@ -4,11 +4,16 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var socket_io = require("socket.io");
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
+
+//socket.io
+var io = socket_io();
+app.io = io;
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -23,7 +28,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/users', users);
+//app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -56,5 +61,10 @@ app.use(function(err, req, res, next) {
   });
 });
 
+// socket.io events
+io.on( "connection", function( socket ){
+
+  console.log( "A user connected" );
+});
 
 module.exports = app;
