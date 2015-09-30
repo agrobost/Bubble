5bbe5b52cbc7877c29f2f2a5d1c29395254e9506
Romain <romain879@gmail.com>
2015-09-30 16:53:16 +0200

    add console color

--- sockets/server/EventReciever.js
+++ sockets/server/EventReciever.js
@@ -2,15 +2,16 @@
 
 var EventReciever = function(IO) {
 	IO.io.on('connection', function (socket) {
-		console.log("[GameServer] Server try to connect with id "+ socket.id);
+		var prefix = "::cyan::[GameServer]::white::"
+		console.log("Server try to connect with id ::green::"+ socket.id,prefix);
 
   		socket.on('disconnect', function () {
-	    	console.log('[GameServer] Server '+ socket.id + ' has been disconnected');
+	    	console.log('Server ::green::'+ socket.id + '::white:: has been disconnected',prefix);
 	    	IO.serverManager.removeServer(socket.id);
 	  	});
 
 	  	socket.on("gameConfig", function(o) {
-	  		console.log("[GameServer] Server "+socket.id+" logged");
+	  		console.log("Server ::green::"+socket.id+"::white:: logged",prefix);
 	  		socket.emit("Validation", {id: socket.id});
 	  		IO.serverManager.addServer(socket.id, o);
 	  	});
